b0ef5ab23a363832fbe0a3e11ad674e84482ec15
kjschelling <kjschelling@gmail.com>
2018-01-15 18:39:09 -0500

    Generated ingredient route
    -ember generate route ingredient
    -linked ingredient to ingredients route
    -in ingredient.js added model hook with params to get ingredient

--- app/router.js
+++ app/router.js
@@ -11,6 +11,7 @@ Router.map(function () {
   this.route('change-password');
   this.route('users');
   this.route('ingredients');
+  this.route('ingredient', {path: 'ingredients/:ingredient_id'});
 });
 
 export default Router;
--- app/routes/ingredients.js
+++ app/routes/ingredients.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // model () { //this is a model hook
+  //   return this.get('store').findAll('ingredient') // findAll makes a GET request to /ingredients
+  // }
+  // actions: {
+  //   createIngredient(ingredient) {
+  //     let newList = this.get('store').createRecord('ingredient', ingredient);
+  //     newList.save();
+  //   }
+  // }
 });

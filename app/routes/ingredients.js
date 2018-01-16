bdd3f837e3120ab0001b543d3fc2c31b55a5f67f
kjschelling <kjschelling@gmail.com>
2018-01-15 19:47:43 -0500

    Created ingredient model
    -used 'ember generate model ingredient name:string note:string' to
    create a model to represent the ingredient data from our API

--- app/routes/ingredients.js
+++ app/routes/ingredients.js
@@ -1,12 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // model () { //this is a model hook
-  //   return this.get('store').findAll('ingredient') // findAll makes a GET request to /ingredients
-  // }
+  model () { //this is a model hook
+    return this.get('store').findAll('ingredient') // findAll makes a GET request to /ingredients
+  }
+
+
   // actions: {
   //   createIngredient(ingredient) {
-  //     let newList = this.get('store').createRecord('ingredient', ingredient);
+  //     let newIngredient = this.get('store').createRecord('ingredient', ingredient);
   //     newList.save();
   //   }
   // }

3b7e7c8a041dcfa84d15d5b85866161d023352ac
kjschelling <kjschelling@gmail.com>
2018-01-18 22:05:42 -0500

    Model.editable working in ingredient.hbs

--- app/models/info.js
+++ app/models/info.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   additionalInfo: DS.attr('string'),
   ingredient: DS.belongsTo('ingredient'),
-  editable: DS.attr('boolean')
+  // editable: DS.attr('boolean')
 });
